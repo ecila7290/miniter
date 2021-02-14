653bcd30fa6682e87b878380daa929c81709dab9
jeehoon kim <jihoon.kim89@gmail.com>
2021-02-14 15:58:55 +0900

    Edit: edited structure to apply layered pattern

--- app.py
+++ app.py
@@ -1,131 +1,23 @@
-import bcrypt
-import jwt
-import datetime
-import flask_cors
+import config
 
-from flask import Flask, jsonify, request, current_app, Response, g
-from sqlalchemy import create_engine, text
-from flask.json import JSONEncoder
-from functools import wraps
-from datetime import datetime, timedelta
+from flask import Flask
+from sqlalchemy import create_engine
+from flask_cors import CORS
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj,set):
-            return list(obj)
-        return JSONEncoder.default(self,obj)
+from model import UserDao, TweetDao
+from service import UserService, TweetService
+from view import create_endpoints
 
-def get_user(user_id):
-    user=current_app.database.execute(text("""
-        SELECT
-            id,
-            name,
-            email,
-            profile
-        FROM users
-        WHERE id=:user_id
-    """),{
-        'user_id':user_id
-    }).fetchone()
-
-    return {
-        'id':user['id'],
-        'name':user['name'],
-        'email':user['email'],
-        'profile':user['profile']
-    } if user else None
-
-def insert_user(user):
-    return current_app.database.execute(text("""
-        INSERT INTO users (
-            name,
-            email,
-            profile,
-            hashed_password
-        ) VALUES (
-            :name,
-            :email,
-            :profile,
-            :password
-        )
-    """), user).lastrowid
-
-def insert_tweet(user_tweet):
-    return current_app.database.execute(text("""
-        INSERT INTO tweets (
-            user_id,
-            tweet
-        ) VALUES (
-            :id,
-            :tweet
-        )
-    """), user_tweet).rowcount
-
-def insert_follow(user_follow):
-    return current_app.database.execute(text("""
-        INSERT INTO users_follow_list(
-            user_id,
-            follow_user_id
-        ) VALUES (
-            :id,
-            :follow
-        )
-    """), user_follow).rowcount
-
-def insert_unfollow(user_unfollow):
-    return current_app.database.execute(text("""
-        DELETE FROM users_follow_list
-        WHERE user_id=:id AND follow_user_id=:unfollow
-    """),user_unfollow)
-
-def get_timeline(user_id):
-    timeline=current_app.database.execute(text("""
-        SELECT DISTINCT
-            t.user_id,
-            t.tweet,
-            t.created_at
-        FROM tweets t
-        LEFT JOIN users_follow_list ufl ON ufl.user_id = :user_id
-        WHERE t.user_id=:user_id
-        OR t.user_id=ufl.follow_user_id
-        ORDER BY -t.created_at
-    """), {
-        'user_id':user_id
-    }).fetchall()
-
-    return [{
-        'user_id':tweet['user_id'],
-        'tweet':tweet['tweet']
-    } for tweet in timeline]
+class Service:
+    pass
 
 ##################################################
-# Decorator
+# Create App
 ##################################################
-def login_required(f):
-    @wraps(f)
-    def decorated_func(*args, **kwargs):
-        access_token=request.headers.get('Authorization')
-        if access_token is not None:
-            try:
-                payload=jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], current_app.config['ALGORITHM'])
-            except jwt.InvalidTokenError:
-                payload=None
-            
-            if payload is None: return Response(status=401)
-
-            user_id=payload['user_id']
-            g.user_id=user_id
-            g.user=get_user(user_id) if user_id else None
-        else:
-            return Response(status=401)
-        
-        return f(*args, **kwargs)
-    return decorated_func
 
 def create_app(test_config=None):
     app=Flask(__name__)
-    flask_cors.CORS(app)
-    app.json_encoder=CustomJSONEncoder
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py')
@@ -133,94 +25,17 @@ def create_app(test_config=None):
         app.config.update(test_config) 
     
     database=create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow=0)
-    app.database=database
 
-    @app.route('/ping', methods=['GET'])
-    def ping():
-        return 'pong'
+    # persistence layer
+    user_dao=UserDao(database)
+    tweet_dao=TweetDao(database)
 
-    @app.route('/signup', methods=['POST'])
-    def sign_up():
-        new_user=request.json
-        new_user['password']=bcrypt.hashpw(new_user['password'].encode('utf-8'),bcrypt.gensalt())
-        new_user_id=insert_user(new_user)
-        new_user=get_user(new_user_id)
+    # business layer
+    services=Service
+    services.user_service=UserService(user_dao,app.config)
+    services.tweet_service=TweetService(tweet_dao)
 
-        return jsonify(new_user)
+    # create endpoint
+    create_endpoints(app,services)
 
-    @app.route('/login', methods=['POST'])
-    def login():
-        credential=request.json
-        email=credential['email']
-        password=credential['password']
-        
-        row=database.execute(text("""
-            SELECT
-                id,
-                hashed_password
-            FROM users
-            WHERE email=:email
-        """), {'email':email}).fetchone()
-        
-        if row and bcrypt.checkpw(password.encode('utf-8'), row['hashed_password'].encode('utf-8')):
-            user_id=row['id']
-            payload={
-                'user_id':user_id,
-                'exp':datetime.utcnow()+timedelta(seconds=60*60*24)
-            }
-            token=jwt.encode(payload, current_app.config['JWT_SECRET_KEY'],current_app.config['ALGORITHM'])
-            
-            return jsonify({'access_token':token})
-        else:
-            return '', 401
-
-    @app.route('/tweet', methods=['POST'])
-    @login_required
-    def tweet():
-        user_tweet=request.json
-        user_tweet['id']=g.user_id
-        tweet=user_tweet['tweet']
-
-        if len(tweet)>300:
-            return 'over 300 characters', 400
-
-        insert_tweet(user_tweet)
-
-        return 'success', 200
-
-    @app.route('/follow', methods=['POST'])
-    @login_required
-    def follow():
-        payload=request.json
-        payload['id']=g.user_id
-        insert_follow(payload)
-
-        return 'success', 200
-    
-    @app.route('/unfollow', methods=['POST'])
-    @login_required
-    def unfollow():
-        payload=request.json
-        payload['id']=g.user_id
-        insert_unfollow(payload)
-
-        return 'success', 200
-
-    @app.route('/timeline/<int:user_id>', methods=['GET'])
-    def timeline(user_id):        
-        return jsonify({
-            'user_id':user_id,
-            'timeline':get_timeline(user_id)
-        })
-    return app
-
-    @app.route('/timeline', methods=['GET'])
-    @login_required
-    def user_timeline():
-        user_id=g.user_id
-        
-        return jsonify({
-            'user_id':user_id,
-            'timeline':get_timeline(user_id)
-        })
     return app
